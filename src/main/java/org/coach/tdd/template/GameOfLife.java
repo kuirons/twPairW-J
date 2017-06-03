1c0c38dcb9145fe1547c42642a04d9f76db70501
kuirons <wugyu@outlook.com>
2017-06-03 10:59:26 +0800

    修改以全部细胞为参数为以数组下标为参数

--- src/main/java/org/coach/tdd/template/GameOfLife.java
+++ src/main/java/org/coach/tdd/template/GameOfLife.java
@@ -2,15 +2,21 @@ package org.coach.tdd.template;
 
 public class GameOfLife {
 
-    public int judgeCellStatus(int[][] test) {
+    private   int stauts[][] = new int[3][3];
+
+    public int[][] getStauts() {
+        return stauts;
+    }
+
+    public int judgeCellStatus(int x, int y) {
         int sum = 0;
-        for (int i = 0; i < test.length; i++) {
-            for (int j = 0; j < test[i].length; j++) {
-                if (test[i][j] == 1) {
+        for (int i = 0; i < stauts.length; i++) {
+            for (int j = 0; j < stauts[i].length; j++) {
+                if (stauts[i][j] == 1) {
                     sum++;
                 }
             }
         }
-        return sum == 2 ? test[1][1] : 0;
+        return sum == 2 ? stauts[x][y] : 0;
     }
 }
--- src/test/java/org/coach/tdd/template/GameOfLifeTest.java
+++ src/test/java/org/coach/tdd/template/GameOfLifeTest.java
@@ -1,26 +1,29 @@
 package org.coach.tdd.template;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class GameOfLifeTest {
     private GameOfLife gameOfLife = new GameOfLife();
-    private int test[][] = new int[3][3];
+    private int test[][] = gameOfLife.getStauts();
+
     @Test
     public void ifCellAroundAllDeadReturnDead() {
-        assertEquals(0, gameOfLife.judgeCellStatus(test));
+        assertEquals(0, gameOfLife.judgeCellStatus(1, 1));
     }
 
     @Test
     public void ifCellAroundHavaOneCellLivedReturnDead() {
         test[0][0] = 1;
-        assertEquals(0, gameOfLife.judgeCellStatus(test));
+        assertEquals(0, gameOfLife.judgeCellStatus(1, 1));
     }
+
     @Test
     public void ifCellAroundHavaTwoCellLivedReturnLastStatus() {
         test[0][0] = 1;
         test[0][1] = 1;
         int status = test[1][1];
-        assertEquals(status, gameOfLife.judgeCellStatus(test));
+        assertEquals(status, gameOfLife.judgeCellStatus(1, 1));
     }
 }
