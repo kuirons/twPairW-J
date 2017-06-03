de6319247a50a0683f0adbe4af8f01f2ac1a3cbc
kuirons <wugyu@outlook.com>
2017-06-03 11:10:42 +0800

    重构：减少return

--- src/main/java/org/coach/tdd/template/GameOfLife.java
+++ src/main/java/org/coach/tdd/template/GameOfLife.java
@@ -10,6 +10,7 @@ public class GameOfLife {
 
     public int judgeCellStatus(int x, int y) {
         int sum = 0;
+        int nextStatus;
         for (int i = 0; i < stauts.length; i++) {
             for (int j = 0; j < stauts[i].length; j++) {
                 if (stauts[i][j] == 1) {
@@ -18,15 +19,16 @@ public class GameOfLife {
             }
         }
         if(sum==2){
-            return stauts[x][y];
+            nextStatus=stauts[x][y];
         }
         else if(sum==3){
             stauts[x][y]=1;
-            return 1;
+            nextStatus=1;
         }
         else{
             stauts[x][y]=0;
-            return 0;
+            nextStatus=0;
         }
+        return nextStatus;
     }
 }
