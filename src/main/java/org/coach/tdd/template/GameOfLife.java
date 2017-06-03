29fe6e4cbca7eb691888c23092ed3aba8cba3dd2
kuirons <wugyu@outlook.com>
2017-06-03 11:18:42 +0800

    完成-情况5:当细胞周围有超过3个细胞存活时，细胞死亡

--- src/main/java/org/coach/tdd/template/GameOfLife.java
+++ src/main/java/org/coach/tdd/template/GameOfLife.java
@@ -2,32 +2,30 @@ package org.coach.tdd.template;
 
 public class GameOfLife {
 
-    private   int stauts[][] = new int[3][3];
+    private int status[][] = new int[3][3];
 
-    public int[][] getStauts() {
-        return stauts;
+    public int[][] getStatus() {
+        return status;
     }
 
     public int judgeCellStatus(int x, int y) {
         int sum = 0;
         int nextStatus;
-        for (int i = 0; i < stauts.length; i++) {
-            for (int j = 0; j < stauts[i].length; j++) {
-                if (stauts[i][j] == 1) {
+        for (int i = 0; i < status.length; i++) {
+            for (int j = 0; j < status[i].length; j++) {
+                if (status[i][j] == 1) {
                     sum++;
                 }
             }
         }
-        if(sum==2){
-            nextStatus=stauts[x][y];
-        }
-        else if(sum==3){
-            stauts[x][y]=1;
-            nextStatus=1;
-        }
-        else{
-            stauts[x][y]=0;
-            nextStatus=0;
+        if (sum == 2) {
+            nextStatus = status[x][y];
+        } else if (sum == 3) {
+            status[x][y] = 1;
+            nextStatus = 1;
+        } else {
+            status[x][y] = 0;
+            nextStatus = 0;
         }
         return nextStatus;
     }
--- src/test/java/org/coach/tdd/template/GameOfLifeTest.java
+++ src/test/java/org/coach/tdd/template/GameOfLifeTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class GameOfLifeTest {
     private GameOfLife gameOfLife = new GameOfLife();
-    private int test[][] = gameOfLife.getStauts();
+    private int test[][] = gameOfLife.getStatus();
 
     @Test
     public void ifCellAroundAllDeadReturnDead() {
@@ -21,8 +21,8 @@ public class GameOfLifeTest {
 
     @Test
     public void ifCellAroundHavaTwoCellLivedReturnLastStatus() {
-        test[0][0] = 1;
         test[0][1] = 1;
+        test[0][2] = 1;
         int status = test[1][1];
         assertEquals(status, gameOfLife.judgeCellStatus(1, 1));
     }
@@ -35,4 +35,13 @@ public class GameOfLifeTest {
         assertEquals(1, gameOfLife.judgeCellStatus(1, 1));
     }
 
+    @Test
+    public void ifCellAroundHavaMoreThanTreeAliveReturnDead() {
+        test[0][0] = 1;
+        test[0][1] = 1;
+        test[0][2] = 1;
+        test[1][2] = 1;
+        test[2][1] = 1;
+        assertEquals(0, gameOfLife.judgeCellStatus(1, 1));
+    }
 }
