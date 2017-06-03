2f63f7f19bece425744dc3626a843fe8df221864
kuirons <wugyu@outlook.com>
2017-06-03 10:17:56 +0800

    完成-情况2：当细胞周围存在一个存活细胞的时候，细胞下一帧状态为死亡

--- src/test/java/org/coach/tdd/template/GameOfLifeTest.java
+++ src/test/java/org/coach/tdd/template/GameOfLifeTest.java
@@ -10,8 +10,16 @@ import java.lang.reflect.Array;
 public class GameOfLifeTest {
     @Test
     public void IfCellAroundAllDeadReturnDead() {
-        int test[][]=new int[3][3];
-        GameOfLife gameoflife=new GameOfLife();
-        assertEquals(0,gameoflife.judgeCellStatus(test));
+        int test[][] = new int[3][3];
+        GameOfLife gameoflife = new GameOfLife();
+        assertEquals(0, gameoflife.judgeCellStatus(test));
+    }
+
+    @Test
+    public void IfCellAroundHavaOneCellLivedReturnDead() {
+        int test[][] = new int[3][3];
+        test[0][0] = 1;
+        GameOfLife gameOfLife = new GameOfLife();
+        assertEquals(0, gameOfLife.judgeCellStatus(test));
     }
 }
